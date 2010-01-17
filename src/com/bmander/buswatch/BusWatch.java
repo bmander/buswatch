24ec145e3a3d551461d9315ff8f576fe4f1a0ae0
Brandon Martin-Anderson <badhill@gmail.com>
2010-01-17 11:32:51 -0800

    stub project as generated by 'android project create'

---
+++ src/com/bmander/buswatch/BusWatch.java
@@ -0,0 +1,15 @@
+package com.bmander.buswatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BusWatch extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
